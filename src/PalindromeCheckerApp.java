56fc6cfb4a32e28049ae695117fe7a2a6bafc394
CHARAN MOPADA <mopadacharan@gmail.com>
2026-03-02 14:51:20 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,14 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application"); // UC1 task
+        System.out.println("Welcome to Palindrome Checker Application");
+
+        String word = "radar";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a palindrome!");
+        } else {
+            System.out.println(word + " is not a palindrome!");
+        }
     }
 }
