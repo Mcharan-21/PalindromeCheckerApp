8658bfa23dcc2d34e2817951ac0faf5acaa12d75
CHARAN MOPADA <mopadacharan@gmail.com>
2026-03-02 15:51:46 +0530

    UC7: Optimized palindrome check using deque

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,31 +1,28 @@
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String original = "level";
-        Queue<Character> queue = new LinkedList<>();
-        Stack<Character> stack = new Stack<>();
+        String original = "rotor";
+        Deque<Character> deque = new LinkedList<>();
 
         for (char ch : original.toCharArray()) {
-            queue.add(ch);
-            stack.push(ch);
+            deque.addLast(ch);
         }
 
         boolean isPalindrome = true;
 
-        while (!queue.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
                 isPalindrome = false;
                 break;
             }
         }
 
         if (isPalindrome) {
-            System.out.println(original + " is a palindrome (using queue & stack).");
+            System.out.println(original + " is a palindrome (using deque).");
         } else {
-            System.out.println(original + " is NOT a palindrome (using queue & stack).");
+            System.out.println(original + " is NOT a palindrome (using deque).");
         }
     }
 }
